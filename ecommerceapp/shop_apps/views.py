57d3e8e8a5f0ab555659c02b6df7ee9c3098cc30
Annie <annie.b@scalgo.net>
2025-06-05 15:31:27 +0530

    created detail serializer

--- ecommerceapp/shop_apps/views.py
+++ ecommerceapp/shop_apps/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from .models import Products
+from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
 
@@ -8,6 +8,6 @@ from rest_framework.response import Response
 
 @api_view(["GET"])
 def products(requests):
-    products = Products.objects.all()
+    products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
