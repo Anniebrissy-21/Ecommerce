76ca2918e68220a2e6de4be9b18dff887c4d0864
Annie Brissy <annie.b@scalgo.net>
2025-08-20 10:40:21 +0530

    gitignore file updated

--- ecommerceapp/env/bin/activate.fish
+++ ecommerceapp/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/annie/fullstack-project/Ecommerce/ecommerceapp/env
+set -gx VIRTUAL_ENV /home/scalgo/Ecommerce/Ecommerce/ecommerceapp/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
